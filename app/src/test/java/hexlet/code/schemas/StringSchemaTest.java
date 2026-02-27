f202417412ab0d6b65b7a809888c6b837162194e
Iurii Rychkov <yuri.rychkov@gmail.com>
2026-02-27 18:40:17 +0100

    Step4

--- app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -1,6 +1,7 @@
 package hexlet.code.schemas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import hexlet.code.Validator;
 import org.junit.jupiter.api.Test;
