249fe7b0c9f30ba88c9f360bca22dcf7542c5b10
Iurii Rychkov <yuri.rychkov@gmail.com>
2026-02-27 18:30:36 +0100

    Step4

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
 class ValidatorTest {
-    Validator validator = new Validator();
+    private Validator validator = new Validator();
 
     @Test
     void getStringSchema() {
--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -1,6 +1,7 @@
 package hexlet.code.schemas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.HashMap;
 import org.junit.jupiter.api.Test;
